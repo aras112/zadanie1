c38fa68d18561b5f44fc4faf02b86d2294c9f736
209354 <aras112nowe@o2.pl>
2019-10-22 16:54:35 +0200

    zadanie 2

---
+++ src/main/java/com/example/demo/PatternEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class PatternEntity {
+    private String napis;
+    private String ciag;
+
+    public String getNapis() {
+        return napis;
+    }
+
+    public void setNapis(String napis) {
+        this.napis = napis;
+    }
+
+    public String getCiag() {
+        return ciag;
+    }
+
+    public void setCiag(String ciag) {
+        this.ciag = ciag;
+    }
+}
--- src/main/java/com/example/demo/Reverse.java
+++ src/main/java/com/example/demo/Reverse.java
@@ -3,12 +3,53 @@ package com.example.demo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RestController
 public class Reverse {
 
-    @GetMapping(value = "/{string}")
-    String reverse(@PathVariable String string){
-        return StringUtils.reverse(string);
+    private Pattern hasSpecialCase = Pattern.compile("[^A-Za-z0-9]");
+    private Pattern hasNumber = Pattern.compile("[0-9]");
+
+    @GetMapping(value = "isMixedCase/{string}")
+    boolean isMixedCase(@PathVariable String string){
+        return StringUtils.isMixedCase(string);
+    }
+
+    @GetMapping(value = "isUpperCase/{string}")
+    boolean isUpperCase(@PathVariable String string){
+        return StringUtils.isAllUpperCase(string);
+    }
+
+    @GetMapping(value = "isLowerCase/{string}")
+    boolean isLowerCase(@PathVariable String string){
+        return StringUtils.isAllLowerCase(string);
+    }
+
+    @GetMapping(value = "hasWhitespaceInString/{string}")
+    boolean hasSpaceInString(@PathVariable String string){
+        return StringUtils.containsWhitespace(string);
+    }
+
+    @GetMapping(value = "hasSpecialCase/{string}")
+    boolean hasSpecialCase(@PathVariable String string){
+        return hasSpecialCase.matcher(string).find();
+    }
+
+    @GetMapping(value = "isNumeric/{string}")
+    boolean isNumeric(@PathVariable String string){
+        return StringUtils.isNumeric(string);
+    }
+
+    @GetMapping(value = "hasNumber/{string}")
+    boolean hasNumber(@PathVariable String string){
+        return hasNumber.matcher(string).find();
+    }
+
+    @PostMapping(value = "/matchPattern")
+    boolean matchPattern(@RequestBody PatternEntity patternEntity){
+        return patternEntity.getNapis().contains(patternEntity.getCiag());
     }
 
 }
