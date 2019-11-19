e4bb3b809baad8ab4bf0c020c49fb65bd67ee9ae
209354 <aras112nowe@o2.pl>
2019-11-19 16:28:44 +0100

    added html response

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -15,12 +15,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
 import java.net.MalformedURLException;
@@ -34,71 +36,17 @@ import java.util.Date;
 public class Controller {
 
 
-    @GetMapping(value = "/holidays", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<Resource> getICSFileHolidays() throws Exception {
-        Document doc = Jsoup.connect("http://www.weeia.p.lodz.pl/").get();
-        Elements swieta = doc.select("a.active");
-        Elements opis = doc.select("div.InnerBox");
-        ICalendar ical = new ICalendar();
+    @GetMapping(value = "/users/{name}")
+    public ResponseEntity<String> getICSFileHolidays(@PathVariable String name) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "https://adm.edu.p.lodz.pl/user/users.php?search=";
 
-        setIcal(ical);
+        Document doc = Jsoup.connect("https://adm.edu.p.lodz.pl/user/users.php?search="+name).get();
 
-        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        int i = 0;
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl + name, String.class);
 
-
-        for (Element e : swieta) {
-
-            VEvent event = new VEvent();
-            event.setSequence(0);
-            event.addAttendee("aras112@o2.pl");
-            event.setCreated(new Date(currentYear, currentMonth, Integer.parseInt(e.text()), 8, 0, 0));
-            event.setDateStart(new Date(currentYear, currentMonth, Integer.parseInt(e.text()), 8, 0, 0));
-            event.setDateEnd(new Date(currentYear, currentMonth, Integer.parseInt(e.text()), 20, 0, 0));
-            event.setStatus(Status.confirmed());
-            event.setUid(Uid.random());
-            event.setDescription(opis.get(i).text());
-            ical.addEvent(event);
-            i++;
-        }
-
-        File file = new File("call.ics");
-        ICalWriter writer = null;
-        try {
-            writer = new ICalWriter(file, ICalVersion.V2_0);
-            writer.write(ical);
-        } finally {
-            if (writer != null) writer.close();
-        }
-
-
-        Resource resource = loadAsResource("call.ics");
-        return new ResponseEntity<Resource>(resource, null, HttpStatus.OK);
+        return response;
     }
-
-    private void setIcal(ICalendar ical) {
-        ical.setDescription("Ważne wydarzenia");
-        ical.setCalendarScale(new CalendarScale("GREGORIAN"));
-        ical.setName("WEEIA CALENDAR");
-        ical.setMethod(Method.REQUEST);
-        ical.setCalendarScale(CalendarScale.gregorian());
-    }
-
-
-    private Resource loadAsResource(String filename) throws FileNotFoundException, MalformedURLException {
-        try {
-            Path file = Paths.get(filename);
-            org.springframework.core.io.Resource resource = new UrlResource(file.toUri());
-            if (resource.exists() || resource.isReadable()) {
-                return (Resource) resource;
-            } else {
-                throw new FileNotFoundException();
-            }
-        } catch (MalformedURLException e) {
-
-            throw new FileNotFoundException();
-        }
-    }
-
 }
